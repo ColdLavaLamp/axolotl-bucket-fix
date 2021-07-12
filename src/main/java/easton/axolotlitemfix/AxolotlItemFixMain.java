470c0f4e1afa5aff0264ba38194304d158af0b57
ColdLavaLamp <85807490+ColdLavaLamp@users.noreply.github.com>
2021-07-12 13:24:24 -0700

    MAVM compat. predicate changes

--- src/main/java/easton/axolotlitemfix/AxolotlItemFixMain.java
+++ src/main/java/easton/axolotlitemfix/AxolotlItemFixMain.java
@@ -2,7 +2,6 @@ package easton.axolotlitemfix;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
@@ -11,13 +10,11 @@ public class AxolotlItemFixMain implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 
-		//float numVars = FabricLoader.getInstance().isModLoaded("mavm") ? 11 : 5;
-
 		FabricModelPredicateProviderRegistry.register(Items.AXOLOTL_BUCKET, new Identifier("variant"), (itemStack, clientWorld, livingEntity, i) -> {
 			float axolotlType = 0;
 			if (itemStack.getTag() != null && itemStack.getTag().contains("Variant"))
 				axolotlType = itemStack.getTag().getInt("Variant");
-			return axolotlType / 10.0f;
+			return axolotlType * 0.01f + 0.0001f;
 		});
 
 		FabricModelPredicateProviderRegistry.register(Items.AXOLOTL_BUCKET, new Identifier("age"), (itemStack, clientWorld, livingEntity, i) -> {
@@ -29,3 +26,4 @@ public class AxolotlItemFixMain implements ClientModInitializer {
 	}
 
 }
+
